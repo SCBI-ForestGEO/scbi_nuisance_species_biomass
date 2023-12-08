9fe5a05e0c9cf1ab9856770de28e1ff4218ba258
krystalbagnaschi <krystalbagnaschi@gmail.com>
2023-12-08 09:40:12 -0500

    Update FluxDataFigureV1.R

--- code/FluxDataFigureV1.R
+++ code/FluxDataFigureV1.R
@@ -1,14 +1,20 @@
-awrtable <- read.csv("/Users/krystalbagnaschi/Documents/15yrsChange/code/biomassRecruitedData.csv")
-awmtable <- read.csv("/Users/krystalbagnaschi/Documents/15yrsChange/code/mortalityFluxData.csv")
+library(ggplot2)
+
+##set your own wd##
+setwd("/Users/krystalbagnaschi/Documents/15yrsChange/")
+
+awrtable <- read.csv("code/biomassRecruitedData.csv")
+awmtable <- read.csv("code/mortalityFluxData.csv")
+awptable <- read.csv("code/WoodyGrowthFluxes.csv")
 
 
-colnames(awrtable) <- c(colnames(awrtable)[1:4],"Value")
-colnames(awmtable) <- c(colnames(awmtable)[1:4],"Value")
+##colnames(awrtable) <- c(colnames(awrtable)[1:4],"Value")
+##colnames(awmtable) <- c(colnames(awmtable)[1:4],"Value")
 
-awrtable[3,4] <- "2018to2023"
+##awrtable[3,4] <- "2018to2023"
 
-biomassfluxtable <- rbind(awrtable, awmtable)
+biomassfluxtable <- rbind(awrtable, awmtable, awgtable)
 
 ggplot(biomassfluxtable, aes(x=Interval, y=Value, group=Flux, col=Flux)) +
   geom_line()
-  
+
