bdc24e5e84f3f3b2aef0b8a4038466d196dd73db
emacmonigle <emacmonigle@gmail.com>
2023-12-12 10:42:04 -0500

    update file names

--- code/figure3_WoodyGrowthFluxData.R
+++ code/figure3_WoodyGrowthFluxData.R
@@ -1,13 +1,13 @@
 #Figure 3 (Woody Growth)
 
-
-###Draft 8 - Set up the dataframe, correctly subset and calculate 2023
-
-### To:do
-    ## Subset the 2023 data by creating a UID for each stem using tag and stemtag and then using the paste function to make a new column
-      ##Ask Luca why your Census_2018_UID is a value
+###Draft 11
 
 #Look at the "Mortality, Growth and Recruitment - Conceptual Differences" word document if you're confused
+
+#Load libraries 
+library(allodb)
+library(dplyr)
+library(tidyverse)
 
 #Read in data from past censuses and the species table
 load(url("https://github.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/raw/master/tree_main_census/data/scbi.stem1.rdata"))
@@ -27,11 +27,12 @@ Census_2013_Species <- merge(Census_2013, Species_table, by ="sp")
 Census_2018_Species <- merge(Census_2018, Species_table, by ="sp")
 
 #Read in data from 2023
-Census_2023 <- read.csv("/Users/rachelhoffman/Documents/GitHub/2023census/processed_data/scbi.stem4.csv")
+Census_2023<- read.csv("data/census data/scbi.stem4.csv")
 #Combine the 2023 census data and species list
 Census_2023_Species <- merge(Census_2023, Species_table, by ="sp")
+
 #Define finished quadrats
-Finished_quadrats <- unique(Census_2023$quadrat)
+Finished_quadrats <- unique(Census_2023_Species$quadrat)
 ##Use this line if you're interested in the biomass for the whole plot = Finished_quadrats <- unique(Census_2018$quadrat)
 subset(Census_2023_Species, Census_2023_Species$quadrat %in% Finished_quadrats)
 Census_2023_Finished <- subset(Census_2023_Species, Census_2023_Species$quadrat %in% Finished_quadrats)
@@ -50,12 +51,6 @@ Census_2013_Finished <- subset(Census_2013_Species, Census_2013_Species$quadrat 
 #Subset the 2018 qudrats by the ones that are finished in 2023
 subset(Census_2018_Species, Census_2018_Species$quadrat %in% Finished_quadrats)
 Census_2018_Finished <- subset(Census_2018_Species, Census_2018_Species$quadrat%in%Finished_quadrats)
-
-
-#Load libraries 
-library(allodb)
-library(dplyr)
-library(tidyverse)
 
 #Calculate biomass for the Censuses
 
@@ -108,45 +103,28 @@ Census_2023_Finished$Calculated_ABG_2023 <- get_biomass(dbh = as.numeric(Census_
 
 
 #2018 to 2023
-    
+    #Create a unique ID for each stem so that you can compare stems from 2023 to 2018
     Census_2018_Finished$UID <- paste(Census_2018_Finished$tag, Census_2018_Finished$StemTag, sep = "_")
     Census_2023_Finished$UID <- paste(Census_2023_Finished$tag, Census_2023_Finished$StemTag, sep = "_")
     
-    #IGNORE ALL OF THIS RIGHT NOW
-    #Check to see if the rows in each census match??
-    #all(Census_2018_Finished$tag == Census_2023_Finished$tag)
-    #all(Census_2018_Finished$StemTag == Census_2023_Finished$StemTag)
-    #Subset out recruits in 2023
-    #Filter by rows that are living in 2018
-    idx <- Census_2018_Finished$status%in%"A" & Census_2023_Finished$status_current%in%"LI"
-    Living_2023 <- Census_2023_Finished[idx, ]
-    Living_2018 <- Census_2018_Finished[idx, ]
-    #Filter by rows that are living in 2023
-    #Living_2023 <- Census_2023_Finished[Census_2023_Finished$status_current == "LI" & Census_2023_Finished$table == "old_trees",]
-    #Filter by rows that are living 2013
-    #Living_2013 <- Census_2013_Finished[Census_2013_Finished$status == "A" & Census_2018_Finished$status == "A",]
-    #Living_2018 <- Census_2018_Finished[Census_2018_Finished$status == "A" & Census_2023_Finished$status_current == "LI",]
-    #Filter by rows that are living in 2018
-    #Living_2023 <- Census_2023_Finished[Census_2023_Finished$status_current == "LI" & Census_2018_Finished$status == "A",]
-      ##Indexing and subsetting get you the same answer  
-    #Filter by rows that are living in 2018
-    #Living_2018 <- Census_2018_Finished[idx, ]
-    Living_2018 <- Census_2018_Finished[Census_2018_Finished == "A" & Census_2023_Complete == "LI"]
-    #Filter by rows that are living in 2023
-    Living_2023 <- Census_2023_Complete[Census_2023_Complete == "LI" & Census_2018_Finished == "A"]
- 
- #Subtract the biomass of the living trees in 2018 from 2023
-  #Woody_growth_2023 <- sum(Living_2023$Calculated_ABG_2023 - Living_2018$Calculated_ABG_2018, na.rm=TRUE)
-    sum_2023 <- sum(Living_2023$Calculated_ABG_2023, na.rm = TRUE)
-    sum_2018 <- sum(Living_2018$Calculated_ABG_2018, na.rm = TRUE)
-    Woody_growth_2023 <- sum_2023 - sum_2018
-    print(Woody_growth_2023)
+    #Create simplified tables
+    Census_2018_Simplified <- Census_2018_Finished[c("UID", "dbh", "status","Genus", "Species", "Calculated_ABG_2018")]
+    Census_2023_Simplified <- Census_2023_Finished[c("UID", "dbh_current", "status_current", "Genus", "Species", "Calculated_ABG_2023")]
+    Merged2018_2023 <- merge(Census_2018_Simplified, Census_2023_Simplified, by="UID", suffixes = c("_2018","_2023"))
+    
+    #Now we can subset!
+    Woody_growth_18_23 <- subset(Merged2018_2023, status%in% "A" & status_current%in% "LI")
+    
+    #Sum the 2018 biomass column and the 2023 biomass column
+    Woody_growth_23 <- sum(Woody_growth_18_23$Calculated_ABG_2023 - Woody_growth_18_23$Calculated_ABG_2018, na.rm = TRUE)
+    print(Woody_growth_23)
     #Convert to megagrams and divide by number of hectares
-    Woody_growth_2023_mg_ha <- Woody_growth_2023/1000/Hectares_measured
+    Woody_growth_2023_mg_ha <- Woody_growth_23/1000/Hectares_measured
     print(Woody_growth_2023_mg_ha)
-    #Find woody growth per year 
+    #Find woody growth per year
     Woody_growth_2023_mg_ha_yr <- Woody_growth_2023_mg_ha/5
     print(Woody_growth_2023_mg_ha_yr)
+    
   
 #Creating a dataframe!
   #Defining objects
@@ -158,7 +136,8 @@ Census_2023_Finished$Calculated_ABG_2023 <- get_biomass(dbh = as.numeric(Census_
   flx3 <- Woody_growth_2023_mg_ha_yr
   
   all_fluxes <- c(flx1, flx2, flx3)
-  Rachels_df <- data.frame(Flux = flux, Year = year, Value = all_fluxes)
+  Woody_growth_df <- data.frame(Flux = flux, Year = year, Value = all_fluxes)
   
-  #write.csv(Rachels_df...pathway)
+  write.csv(Woody_growth_df, file = "doc/woody_growth_df.csv")
+  
   
