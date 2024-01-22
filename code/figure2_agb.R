057b6ef51f6ebcdba4329fc7e57803b7d8f7606c
Luca L. Morreale <llm77@cornell.edu>
2024-01-22 18:16:33 -0500

    region identification, figure 2 adjustments

--- code/figure2_agb.R
+++ code/figure2_agb.R
@@ -1,6 +1,7 @@
 #figure 2 ABG (Above Ground Biomass)
 #output Line graph of aboveground biomass stocks
 #december 13th, 2023
+#Updated by LLM on 1/22/2024
 
 #clear environment
 rm(list = ls())
@@ -10,6 +11,8 @@ library(allodb)
 library(ggplot2)
 
 #Read in data from past censuses and the species table
+grouped_quadrats <- read.csv("C:/Work/Smithsonian/Repos/15yrsChange/data/grouped_quadrats.csv")  %>% 
+  mutate(quadrat = sprintf("%04d",quadrat))
 load(url("https://github.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/raw/master/tree_main_census/data/scbi.stem1.rdata"))
 load(url("https://github.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/raw/master/tree_main_census/data/scbi.stem2.rdata"))
 load(url("https://github.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/raw/master/tree_main_census/data/scbi.stem3.rdata"))
@@ -35,16 +38,15 @@ spCensus2023$quadrat <- sprintf("%04d", spCensus2023$quadrat)
 latlong <- c(-78.1454, 38.8935)
 
 #######################################################################################################################################
-                                                #for unfinished census data
-
+                                          
 #Define finished quadrats
-Finished_quadrats <- unique(spCensus2023$quadrat)
+#Finished_quadrats <- unique(spCensus2023$quadrat)   #for unfinished census data
+Finished_quadrats <- unique(spCensus2018$quadrat)  #Use this line if you're interested in the biomass for the whole plot
 
 #Finding the physical amount of space we measured
 Hectares_measured <- length(Finished_quadrats)*20*20/10000
 
-###Use this line if you're interested in the biomass for the whole plot = Finished_quadrats <- unique(Census_2018$quadrat)
-##Census_2023_Finished <- subset(spCensus2023, spCensus2023$quadrat %in% Finished_quadrats)
+#Census_2023_Finished <- subset(spCensus2023, spCensus2023$quadrat %in% Finished_quadrats)
 
 #Subset the 2008 quadrats by ones that are finished in 2023
 Census_2008_Finished <- subset(spCensus2008, spCensus2008$quadrat %in% Finished_quadrats)
@@ -58,23 +60,11 @@ Census_2018_Finished <- subset(spCensus2018, spCensus2018$quadrat %in% Finished_
 #rename 2023 census to match names of others
 Census_2023_Finished <- spCensus2023
 
-#######################################################################################################################################
-#debuggging
-
-Finished_quadrats <- unique(spCensus2023$quadrat)
-finished2008 <- unique(Census_2008_Finished$quadrat)
-
-Finished_quadrats <- order(Finished_quadrats)
-finished2008 <- order(finished2008)
-missingQuad <- cbind(Finished_quadrats, finished2008) 
-
-
-#######################################################################################################################################
-
-useCensus2008 <- Census_2008_Finished
-useCensus2013 <- Census_2013_Finished
-useCensus2018 <- Census_2018_Finished
-useCensus2023 <- Census_2023_Finished
+#subset to alive stems
+useCensus2008 <- Census_2008_Finished  %>% filter(DFstatus %in% "alive")  
+useCensus2013 <- Census_2013_Finished  %>% filter(DFstatus %in% "alive")
+useCensus2018 <- Census_2018_Finished  %>% filter(DFstatus %in% "alive")
+useCensus2023 <- Census_2023_Finished  %>% filter(status_current %in% "LI")
 
 #calculate total biomass of census year by each stem 
 #biomass_census <- get_biomass(dbh = as.numeric(Census_Year_Complete$dbh)/10, genus = Census_Year_Complete$Genus, species = Census_Year_Complete$Species, coords = latlong)
@@ -83,22 +73,44 @@ useCensus2013$Calculated_ABG <- get_biomass(dbh = as.numeric(useCensus2013$dbh)/
 useCensus2018$Calculated_ABG <- get_biomass(dbh = as.numeric(useCensus2018$dbh)/10, genus = useCensus2018$Genus, species = useCensus2018$Species, coords = latlong)
 useCensus2023$Calculated_ABG <- get_biomass(dbh = as.numeric(useCensus2023$dbh_current)/10, genus = useCensus2023$Genus, species = useCensus2023$Species, coords = latlong)
 
-stock2008 <- sum(useCensus2008$Calculated_ABG, na.rm = TRUE)/25.6 #sum of total kg/hectare
-stock2013 <- sum(useCensus2013$Calculated_ABG, na.rm = TRUE)/25.6
-stock2018 <- sum(useCensus2018$Calculated_ABG, na.rm = TRUE)/25.6
-stock2023 <- sum(useCensus2023$Calculated_ABG, na.rm = TRUE)/25.6
 
-#creating the data frame
+calc_quadrat_abg <- function(abg_df, year){
+  outdf <- abg_df  %>% 
+    group_by(quadrat)  %>% 
+    summarize(Abg_C_Mg = sum(Calculated_ABG,na.rm = T)/1000 * .47)  %>% 
+    left_join(grouped_quadrats  %>% select(quadrat,Group))  %>% 
+    mutate(Year = year)
+  return(outdf)
+}
+quadrat_abg_2008 <- calc_quadrat_abg(useCensus2008,2008) 
+quadrat_abg_2013 <- calc_quadrat_abg(useCensus2013,2013)
+quadrat_abg_2018 <- calc_quadrat_abg(useCensus2018,2018)
+quadrat_abg_2023 <- calc_quadrat_abg(useCensus2023,2023)  %>% 
+  filter(!is.na(as.numeric(quadrat)))  %>% 
+  mutate(quadrat = sprintf("%04d", as.numeric(quadrat)))
 
-year <- c(2008, 2013, 2018, 2023)
-all_stocks <- c(stock2008, stock2013, stock2018, stock2023)
+group_trends <- quadrat_abg_2008  %>% 
+  bind_rows(quadrat_abg_2013,quadrat_abg_2018,quadrat_abg_2023)  %>% 
+  group_by(Year,Group)  %>% 
+  summarize(n_quads = n(),Abg_C_Mg_Ha = sum(Abg_C_Mg) / ((400 * n_quads)/10000))  %>% 
+  drop_na()  %>% 
+  mutate(Group = as.character(Group))
+tot_trends <- quadrat_abg_2008  %>% 
+  bind_rows(quadrat_abg_2013,quadrat_abg_2018,quadrat_abg_2023)  %>% 
+  group_by(Year)  %>% 
+  summarize(n_quads = n(),Abg_C_Mg_Ha = sum(Abg_C_Mg) / ((400 * n_quads)/10000))  %>% 
+  mutate(Group = "Whole Plot")
 
-figure2_agb <- data.frame(y = year,  x = all_stocks)
+figure2_agb <- group_trends  %>% 
+  bind_rows(tot_trends)
  
-ggplot(figure2_agb, aes(y=all_stocks, x=year)) +
+ggplot(figure2_agb, aes(y=Abg_C_Mg_Ha, x=Year, group = Group, col = Group)) +
   geom_line() +
   geom_point() +
-  labs(title = "Carbon Stock Per Year", y = "Carbon Stock (mg/ha)", x = "Year") +
-  theme(legend.position="top")
+  labs(y = "Carbon Stock (Mg C/Ha)", x = "Year") +
+  theme_bw() +
+  theme(legend.position ="bottom",
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 18))  
 
 
--- code/identify-plot-regions.r
+++ code/identify-plot-regions.r
@@ -2,7 +2,7 @@ library(sf)
 library(tidyverse)
 library(terra) 
 library(tidyterra)
-#library("dendextend")
+
 
 setwd("C:/Work/Smithsonian/Repos/SCBI-ForestGEO-Data/")
 load("tree_main_census/data/scbi.stem1.rdata")
@@ -12,7 +12,8 @@ quadrats <- st_read("spatial_data/shapefiles/20m_grid.shp")
 dem <- rast("spatial_data/elevation/rasters/plot_elevation.tif")
 deer_exclosure <- st_read("spatial_data/shapefiles/deer_exclosure_2011.shp")
 twi <- rast("spatial_data/elevation/rasters/plot_TWI.tif")
-#twi_quad <- rast("spatial_data/elevation/rasters/plot_TWI_quadrat.tif")
+
+##### Remove lowland quadrats #####
 twi_nostreams <- twi
 twi_nostreams[twi_nostreams > 10] <- NA
 #plot(twi_nostreams)
@@ -20,20 +21,21 @@ twi_nostreams[twi_nostreams > 10] <- NA
 quadrats <- quadrats  %>% 
     mutate(TWI = unlist(zonal(twi_nostreams,vect(quadrats), fun = mean, na.rm = F)))
 
-###plot TWI with cut-off of 10###
-twi_fig <- ggplot() +
+##plot TWI with cut-off of 10##
+ggplot() +
     geom_spatraster(data  = twi_nostreams) +
+   # geom_sf(data = quadrats,aes( fill = TWI)) + 
 #    geom_spatraster(data  = twi_quad) +
     scale_fill_whitebox_c(palette = "muted") +
     geom_sf(data = deer_exclosure, colour = "black", fill = NA,lwd = 1.5) +
    # geom_sf(data = quadrats, colour = "black", fill = NA,lwd = 1.5) +
     theme_classic()
 
-ggsave(twi_fig,filename = "C:/Work/Smithsonian/Repos/15yrsChange/doc/nondisplay-figs/TWI_nostreams.jpeg", units = "in",width = 5, height = 6)
 
 
+##### get Basal Area of canopy species #####
 
-### get Basal Area of canopy species ###
+affected_species <- c("fram","frni","frpe","frsp","ulam","ulru","ulsp","juci","cade") # species affected by pests & pathogens
 stem.sp <- scbi.stem1  %>% 
     left_join(scbi.spptable)
 
@@ -42,120 +44,49 @@ canopy_sp <- stem.sp  %>%
     mutate(BA = pi * ((as.numeric(dbh) / 10)^2))  %>% 
 #    group_by(quadrat,Genus)  %>% 
     group_by(quadrat,sp)  %>% 
-    summarize(BA = sum(BA))  
+    summarize(BA = sum(BA))  %>% 
+    mutate(affected = if_else(sp %in% affected_species, "Y","N"))
 
-wide_canopy  <- canopy_sp  %>% 
-    ungroup()  %>% 
-    #pivot_wider(id_cols = quadrat,names_from = Genus,values_from = BA)  %>% 
-    pivot_wider(id_cols = quadrat,names_from = sp,values_from = BA)  %>% 
-    replace(is.na(.),0)
-
-####Clustering####
-km <- kmeans(wide_canopy[,-1],centers = 4) 
-
-can_distmat <- vegan::vegdist(wide_canopy[,-1], method = "euclidean")
-ward.hc <- hclust(can_distmat,method = "ward.D2")
-dend <- as.dendrogram(ward.hc)
-dend <- dendextend::color_branches(dend, k = 4)
-dend <- dendextend::color_labels(dend,k=4)
-jpeg(filename = "C:/Work/Smithsonian/Repos/15yrsChange/doc/nondisplay-figs/Cluster-dendrograms.jpeg")
-plot(dend)
-dev.off()
-
-ward.clust <- cutree(ward.hc,k = 4)
-quad_clust <- wide_canopy  %>% 
-    mutate(kmclust = km$cluster,wardclust = ward.clust)   %>% 
-    pivot_longer(cols = acne:coal,names_to = "sp",values_to = "BA")  %>% 
-    group_by(wardclust,sp)  %>% 
-    summarize(BA = sum(BA))  %>%
-    mutate(propBA = BA/ sum(BA))  %>% 
-    arrange(wardclust,desc(propBA))  %>% 
-    slice_head(n = 10)
-#    mutate(maxval = names(.)[max.col(.)])
-#    select(quadrat,kmclust,wardclust)  
-#table(quad_clust$wardclust,quad_clust$maxval)
-
-#plot constitutive parts of ward clusters
-clust_sp_fig <- ggplot(quad_clust,aes(x = wardclust, y = propBA, fill = sp, label = sp)) + 
-    geom_bar(position = position_dodge(width = .9),stat = "identity") + 
-    geom_text(position = position_dodge(width = .9), vjust = -1, size = 3) +
-    scale_fill_manual(values = viridis::plasma(n=14)) +
-    labs(x = "Cluster #",y = "Proportion of Basal Area") +
-    theme_classic() +
-    theme(axis.title = element_text(size = 16),
-         axis.text = element_text(size = 14))
-
-ggsave(clust_sp_fig,filename = "C:/Work/Smithsonian/Repos/15yrsChange/doc/nondisplay-figs/cluster_species.jpeg", units = "in",width = 12, height = 6)
+canopy_binary <- canopy_sp  %>% 
+    group_by(quadrat, affected)  %>% 
+    summarize(BA = sum(BA,na.rm = T))  %>% 
+    mutate(propBA = BA / sum(BA))  %>% 
+    filter(affected %in% ("Y"))  %>% 
+    select(quadrat, propAffected = propBA)
 
 
 
-clust_join <- wide_canopy  %>% 
-    mutate(clust = ward.clust)  %>% 
-    select(quadrat,clust)
-##############
-
-canopy_max <- canopy_sp  %>% 
-    ungroup()  %>% 
-    group_by(quadrat)  %>% 
-    filter(BA == max(BA)) %>% 
-    left_join(clust_join)
-
-View(quad_clust)
-
-quadrats_canopy<- quadrats  %>% 
+##### create column with binary deer-exclosure variable #####
+quad_affected <- quadrats  %>% 
     mutate(quadrat = sprintf("%04d",PLOT))  %>% 
-    left_join(canopy_max)  
- 
-plotdf <- quadrats_canopy  %>% 
-    #mutate(sp = if_else(is.na(TWI),NA,Genus))
-    mutate(clust = if_else(is.na(TWI),NA,clust))
-
-# library(basemaps)
-# ext <- st_bbox(plotdf)
-# bm <- basemap(ext = ext, map_service = "esri",map_type = "world_imagery", class = "terra")
-
-quadrat_clusts_fig <- ggplot() +
-#    geom_spatraster_rgb(data = bm) +
-    geom_sf(data  = plotdf,aes( fill = as.factor(clust))) +
-    scale_fill_manual("Cluster ID",values = viridis::plasma(n = 4),na.value = "grey70") +
-    geom_sf(data = deer_exclosure, colour = "black", fill = NA,lwd = 1.5) +
-   # geom_sf(data = quadrats, colour = "black", fill = NA,lwd = 1.5) +
-    theme_classic() +
-    theme(legend.title = element_text(size = 14),
-          legend.text = element_text(size = 12))
-
-ggsave(quadrat_clusts_fig,filename = "C:/Work/Smithsonian/Repos/15yrsChange/doc/nondisplay-figs/quadrat_clusterID.jpeg", units = "in",width = 6, height = 8)
-
-### create column with binary deer-exclosure variable ###
-
-#intersect_pct <- 
-
-intersect_pct <- st_intersection(plotdf, deer_exclosure)  %>% 
+    left_join(canopy_binary)  %>% 
+    mutate(propAffected = if_else(is.na(propAffected),0,propAffected))
+intersect_pct <- st_intersection(quad_affected, deer_exclosure)  %>% 
     mutate(intersect_area = st_area(.))  %>% 
     select(quadrat,intersect_area)  %>% 
     st_drop_geometry() 
-deer_id <- plotdf  %>% 
+deer_id <- quad_affected  %>% 
     left_join(intersect_pct)  %>% 
     mutate(coverage = as.numeric(intersect_area / 400),coverage = if_else(is.na(coverage),0,coverage), within_deer = if_else(coverage >= .7,"Yes","No"))  %>% 
-    select(quadrat,within_deer) # %>% 
-#plot(deer_id["within_deer"])
+    select(quadrat,TWI,propAffected,within_deer) # %>% 
 
-######
-out_quads <- plotdf  %>% 
-    select(quadrat,clust)  %>% 
-    left_join(deer_id %>% st_drop_geometry())  %>% 
-    st_drop_geometry()  
-
-
-deer_exclos_fig <- ggplot() +
-    geom_sf(data  = deer_id,aes( fill = as.factor(within_deer))) +
-    geom_sf(data = deer_exclosure, colour = "black", fill = NA,lwd = 1.5) +
-    scale_fill_manual(">70% within\n Deer Exclosure",values = rev(viridis::magma(n = 2)),na.value = "grey70") +
+##### Group quadrats into subregions #####
+grouped_quads <- deer_id  %>% 
+    mutate(Group = if_else(within_deer %in% c("Yes"),1,
+        if_else(within_deer %in% c("No") & propAffected < .2,2,
+        if_else(within_deer %in% c("No") & propAffected > .2,3,99))),
+        Group = if_else(is.na(TWI),NA, Group))  
+plot_regions <- ggplot() +
+    geom_sf(data  = grouped_quads,aes( fill = as.factor(Group))) +
+    scale_fill_manual("Group ID",values = c("#017161","#4c90b0","darkblue"),na.value = "transparent") +
+    geom_sf(data = grouped_quads, colour = "black", fill = NA,lwd = .9) +
     theme_classic() +
     theme(legend.title = element_text(size = 14),
           legend.text = element_text(size = 12))
 
-ggsave(deer_exclos_fig,filename = "C:/Work/Smithsonian/Repos/15yrsChange/doc/nondisplay-figs/deer_exclosure_fig.jpeg", units = "in",width = 6, height = 8)
+ggsave(plot_regions,filename = "C:/Work/Smithsonian/Repos/15yrsChange/doc/nondisplay-figs/plot_regions_fig.jpeg", units = "in",width = 6, height = 8)
+out_quads <- grouped_quads  %>% 
+    select(quadrat,Group)  %>% 
+    st_drop_geometry()  
 
-
-write.csv(out_quads,"C:/Work/Smithsonian/Repos/15yrsChange/data/processed_data/clustered_quadrats.csv")
+write.csv(out_quads,"C:/Work/Smithsonian/Repos/15yrsChange/data/grouped_quadrats.csv")
