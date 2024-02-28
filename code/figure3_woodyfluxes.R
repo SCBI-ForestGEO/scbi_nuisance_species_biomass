55beffbef93488ac4c2e0fff86729f550b480f02
rmh24 <rmhoffman24@gmail.com>
2024-02-28 15:41:59 -0500

    Update Figure 3

--- code/figure3_woodyfluxes.R
+++ code/figure3_woodyfluxes.R
@@ -12,7 +12,7 @@ group_trends <- woodyfluxes  %>%
   group_by(Group, Census)  %>% 
   summarize(qc = n(),
             AWP = sum(AWP) / qc, 
-            AWM = (sum(AWM) / qc ) * -1, 
+            AWM = (sum(AWM) / qc ), 
             AWR = sum(AWR) / qc, 
             NetFlux = sum(NetFlux) / qc) %>% 
   mutate(Group = as.character(Group))
@@ -22,7 +22,7 @@ tot_trends <- woodyfluxes  %>%
   group_by(Census)  %>% 
   summarize(qc = n(),
             AWP = sum(AWP) / qc, 
-            AWM = (sum(AWM) / qc ) * -1, 
+            AWM = (sum(AWM) / qc ),  
             AWR = sum(AWR) / qc, 
             NetFlux = sum(NetFlux) / qc)  %>% 
   mutate(Group = "Whole Plot")
@@ -37,23 +37,31 @@ fig3_woodyfluxes <- group_trends  %>%
          cens_int = case_when(Census == 2 ~ "2008-\n2013",
                               Census == 3 ~ "2013-\n2018",
                               Census == 4 ~ "2018-\n2023")
-         )
+  )
 
-colz <- c("#017161","#4c90b0","darkblue","#754792")
+#colz <- c("#017161","#4c90b0","darkblue","#754792")
+colz <- c("#aa5274","#8578a7","#3A6F9E","#b8d89b")
+#colz <- c("#750000","#C7622B","#E7BC40", "#7e937f")
+#colz <- c("#b30000","#326634","#637eb6", "#000000")
+
+
+###linetype <- c("2", "3", "3", "1")
+
 flux_names <- as_labeller(c("AWM" = "Aboveground Woody Mortality",
                             "AWP" = "Aboveground Woody Productivity",
                             "AWR" = "Aboveground Woody Recruitment",
-                            "NetFlux" = "Aboveground Net Woody Production "))
+                            "NetFlux" = "Net Biomass Change"))
 
 fig3 <- ggplot(fig3_woodyfluxes, aes(x = as.ordered(cens_int), y = MgC_Yr_Ha, group = Group, col = Group)) +
- facet_wrap(~Flux, scales = "free_y", labeller = flux_names) +
- geom_line(lwd = 1)+ 
- geom_line(data = fig3_woodyfluxes  %>% filter(Group %in% c("Whole Plot")), lwd = 3) +
- geom_point(cex = 2) + 
- geom_point(data = fig3_woodyfluxes  %>% filter(Group %in% c("Whole Plot")), cex = 5) +
- geom_hline(data = fig3_woodyfluxes  %>% filter(Flux %in% c("NetFlux")),aes(yintercept = 0), lty = "dashed") +
- scale_color_manual(name = element_blank(),labels = c("Low deer, low nuisance","High deer, low nuisance", "High deer, high nuisance", "Plot"),values = colz) + 
- labs(y = expression("Carbon Flux"~(Mg~C~Ha^-1~Yr^-1)), x = "Census") +
+  facet_wrap(~Flux, scales = "free_y", labeller = flux_names) +
+  geom_line(lwd = 1)+ 
+  ###geom_line(data = fig_woodyfluxes %>% filter(Group %in% c("2", "3")), lty = 2)) + 
+  ###geom_line(data = fig_woodyfluxes %>% filter(Group %in% c("1")), lty = 3)
+  geom_point(cex = 2) + 
+  geom_hline(data = fig3_woodyfluxes  %>% filter(Flux %in% c("NetFlux")),aes(yintercept = 0), lty = "dashed") +
+  scale_color_manual(name = element_blank(),labels = c("Low deer, low nuisance","High deer, low nuisance", "High deer, high nuisance", "Plot"),values = colz) +
+  ###scale_linetype_manual(name = element_blank(), labels = c("Low Deer, low nuisance", "High deer, low nuisance", "High deer, high nuisance", "Plot"), values = linetype) +
+  labs(y = expression("Carbon Flux"~(Mg~C~Ha^-1~Yr^-1)), x = "Census") +
   theme_bw() +
   theme(legend.position = "top",
         legend.text = element_text(size = 14),
@@ -63,7 +71,9 @@ fig3 <- ggplot(fig3_woodyfluxes, aes(x = as.ordered(cens_int), y = MgC_Yr_Ha, gr
         axis.text.y = element_text(size = 18, margin = margin(t = 0,r = .08, b = 0,l = 0, unit = "in")),
         axis.title = element_text(size = 20))  
 
+print(fig3)
+
 
 ggsave(fig3,filename = "C:/Work/Smithsonian/Repos/15yrsChange/doc/display/Figure3.jpeg", units = "in",
-        height = 8, width = 10, dpi = 300)
+  height = 8, width = 10, dpi = 300)
 
