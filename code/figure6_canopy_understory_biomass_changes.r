dcbcdabd75a431710ab05c59a3f1146c79223800
Luca L. Morreale <llm77@cornell.edu>
2024-03-11 17:47:44 -0400

    Figure 6 changes

--- code/figure6_canopy_understory_biomass_changes.r
+++ code/figure6_canopy_understory_biomass_changes.r
@@ -8,30 +8,26 @@ grouped_quadrats <- read.csv("C:/Work/Smithsonian/Repos/15yrsChange/data/grouped
 load("C:/Work/Smithsonian/Repos/15yrsChange/data/census_data/all_censuses_agb.rdata")
 deer_exclosure <- st_read("C:/Work/Smithsonian/Repos/SCBI-ForestGEO-Data/spatial_data/shapefiles/deer_exclosure_2011.shp")
 quadrat_shp <- read_sf("data/20m_grid/20m_grid.shp")  
-grouped_quadrats <- read.csv("data/grouped_quadrats.csv") %>%
-  mutate(quadrat = sprintf("%04d", quadrat))  %>% 
-  select(-X)
 spTable <- read.csv("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/species_lists/Tree%20ecology/SCBI_ForestGEO_sp_ecology.csv")
 
 missing <- data.frame(sp = c("acsp","casp","crsp","frsp","prsp","ulsp"), canopy_position = "canopy")
 
 canopyposition <- spTable  %>% 
   select(spcode,canopy_position, life_form)  %>% 
-  mutate(canopy_position_new = if_else(life_form %in% c("tree"),"canopy","understory"))  %>%
+  mutate(canopy_position_new = case_when(spcode %in% "acne" ~ "understory",
+                                         life_form %in% c("tree") ~ "canopy",
+                                        .default = "understory"))  %>%  
   select(sp = spcode, canopy_position = canopy_position_new)  %>% 
   bind_rows(missing)
 
 # sort species by canopy vs non-canopy status 
-all_censuses  %>% 
-  left_join(canopyposition)  %>% 
-  View()
 
-dbh_cutoff <- 50
+dbh_cutoff <- 30
 
 canopy_abg <- all_censuses  %>% 
   left_join(canopyposition)  %>% 
   filter(DFstatus %in% c("alive"))  %>%
-  filter(canopy_position %in% "canopy" & dbh >= dbh_cutoff)  %>%  
+  filter(canopy_position %in% "canopy" & as.numeric(dbh) >= dbh_cutoff)  %>%  
   mutate(quadrat = factor(quadrat), Census = factor(Census))  %>% 
   group_by(quadrat, Census, .drop = FALSE)  %>% 
   summarize(WoodyAGB = sum(ABG,na.rm = T) / 1000 / .04 * .47)
@@ -39,7 +35,7 @@ canopy_abg <- all_censuses  %>%
 understory_abg <- all_censuses  %>% 
   left_join(canopyposition)  %>% 
   filter(DFstatus %in% c("alive"))  %>%
-  filter(canopy_position %in% "canopy" & dbh < dbh_cutoff)  %>%  
+  filter(canopy_position %in% "canopy" & as.numeric(dbh) < dbh_cutoff)  %>%  
   mutate(quadrat = factor(quadrat), Census = factor(Census))  %>%
   group_by(quadrat, Census,.drop = FALSE)  %>% 
   summarize(WoodyAGB = sum(ABG,na.rm = T) / 1000 / .04 * .47)
@@ -59,20 +55,24 @@ canopy_slopes <- canopy_trends  %>%
   filter(term %in% "Census")  %>% 
   select(quadrat, canopy_trend = estimate)
 
-understory_trends <- understory_abg  %>% 
-  filter(Census %in% c(1,4))  %>%  
-  mutate(Census = as.numeric(Census))  %>%  
-  group_by(quadrat)  %>% 
-  group_modify(~ broom::tidy(lm(WoodyAGB ~ Census, data = .x)))
+# understory_trends <- understory_abg  %>% 
+#   filter(Census %in% c(1,4))  %>%  
+#   mutate(Census = as.numeric(Census))  %>%  
+#   group_by(quadrat)  %>% 
+#   group_modify(~ broom::tidy(lm(WoodyAGB ~ Census, data = .x)))
 
-understory_slopes <-  understory_trends %>% 
-  filter(term %in% "Census" )  %>% 
-  select(quadrat, understory_trend = estimate)
+understory_biomass <- understory_abg  %>% 
+  filter(Census %in% c(4)) 
+
+# understory_slopes <-  understory_trends %>% 
+#   filter(term %in% "Census" )  %>% 
+#   select(quadrat, understory_trend = estimate)
   
 ##### Plotting #####
-fillcols <- c("#b7c3e0", "#f3f2f2", "#ffc080","#85a4eb", "#e2dede", "#fd9e3f","#004494", "#a6a6a6", "#cc4c00")
-bivariate_color_scale <- tibble(understory = rep(3:1, 3),
-                                canopy = rep(1:3, each = 3),
+#fillcols <- c("#b7c3e0", "#f3f2f2", "#ffc080","#85a4eb", "#e2dede", "#fd9e3f","#004494", "#a6a6a6", "#cc4c00")
+fillcols <- c("#85a4eb","#004494", "#e2dede", "#a6a6a6","#fd9e3f", "#cc4c00")
+bivariate_color_scale <- tibble(understory = rep(2:1, 3),
+                                canopy = rep(1:3, each = 2),
                                 fill = fillcols)
 
 # bivariate_color_scale <- tibble(
@@ -96,23 +96,27 @@ bivariate_color_scale <- tibble(understory = rep(3:1, 3),
 plotdf <- quadrat_shp  %>% 
   mutate(quadrat = sprintf("%04d", PLOT))  %>% 
   left_join(canopy_slopes)  %>% 
-  left_join(understory_slopes)  %>% 
+  left_join(understory_biomass)  %>% 
+  mutate(WoodyAGB = if_else(is.na(WoodyAGB),0,WoodyAGB))  %>% 
   mutate(canopy = case_when(canopy_trend >= 3 ~ 3,
                               canopy_trend < 3 & canopy_trend > -3 ~ 2,
                               canopy_trend <= -3 ~ 1),
-         understory = case_when(understory_trend >= 1 ~ 3,
-                                understory_trend < 1 & understory_trend >= -1 ~ 2, 
-                                understory_trend < -1 ~ 1))  %>% 
+         understory = case_when(WoodyAGB >= .02 ~ 2,
+                                WoodyAGB < .02 ~ 1 ))  %>% 
   left_join(bivariate_color_scale) 
 
 map <- ggplot(data = plotdf) +
     scale_alpha(name = "",range = c(.6,0),guide = F) +
     geom_sf(aes(fill = fill),color = "white",size = .1) +
-#    geom_sf(data = deer_exclosure, colour = "black", fill = NA,lwd = 1.5) +
+    geom_sf(data = deer_exclosure, colour = "black", fill = NA,lwd = 1.5) +
     scale_fill_identity() +
     # geom_sf(data = deer_exclosure, colour = "black", fill = NA,lwd = 1.5) +
     theme_classic() +
-    theme(plot.margin = unit(c(0,0,0,0), "cm"))
+    theme(plot.margin = unit(c(0,0,0,10), "cm"),
+          legend.position = "none",
+          axis.line = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank())
 
 
 legend <- ggplot() +
@@ -126,7 +130,7 @@ legend <- ggplot() +
   scale_fill_identity() +
   labs(y = "Canopy biomass ⟶️",
        x = "Understory biomass \n of canopy sp. ⟶️") +
-  scale_x_discrete(labels  = c("1" = "Decreasing","2" = "Stable","3" = "Increasing")) +
+  scale_x_discrete(labels  = c("1" = "Low","2" = "High")) +
   scale_y_discrete(labels  = c("1" = "Decreasing","2" = "Stable", "3" = "Increasing")) +
   theme_classic() +
   # make font small enough
@@ -162,22 +166,29 @@ barp_df <- group_counts  %>%
   bind_rows(wplot_counts)  %>% 
   left_join(bivariate_color_scale)
 
-barp <- ggplot(barp_df, aes(x = as.character(Group), y = pct_area,group = plot_group ,fill = fill)) +
-  geom_bar(stat = "identity") +
+barp_df2 <- barp_df  %>% 
+  group_by(Group, understory)  %>% 
+  summarize(pct_area = sum(pct_area))
+
+barp <- ggplot(barp_df) +
+  geom_bar(stat = "identity",aes(x = as.character(Group), y = pct_area,group = plot_group ,fill = fill)) +
+  geom_bar(data = barp_df2, stat = "identity", aes( x = as.character(Group), y = pct_area,group = factor(understory)), col = "white", fill = "transparent", lwd = 1.2) +
   geom_vline(xintercept = 3.5) +
   theme_classic() +
   scale_fill_identity() +
   ylab("% Area") +
   scale_x_discrete(labels  = c("1" = "Low deer,\nlow vulnerable species","2" = "High deer,\n low vulnerable species","3" = "High deer,\n high vulnerable species")) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+  theme(axis.text = element_text(angle = 45, hjust = 1, size = 16),
+        axis.title.y = element_text(size = 18),
         axis.title.x = element_blank())
 fig6 <- cowplot::ggdraw() +
   cowplot::draw_plot(map, 0, 0, 1, 1) +
-  cowplot::draw_plot(legend, 0.001,0.6, 0.25, 0.3) +
-  cowplot::draw_plot(barp, 0.0001,  0.0001, .3,.6)
+  cowplot::draw_plot(legend, x = 0.15, y = 0.6, width = 0.25, height = 0.3) +
+  cowplot::draw_plot(barp, x = .10,  y = 0.0001,width =  .35, height = 0.6)
 fig6
 
-ggsave(fig6,filename = "C:/Work/Smithsonian/Repos/15yrsChange/doc/display/Figure6.jpeg", units = "in", height = 6, width = 8, dpi = 300)
+
+ggsave(fig6,filename = "C:/Work/Smithsonian/Repos/15yrsChange/doc/display/Figure6.jpeg", units = "in", height = 8, width = 10, dpi = 300)
 
 
 
