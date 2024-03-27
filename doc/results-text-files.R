4b6231995a44d662273ea64c932c8beef974a2e2
rmh24 <rmhoffman24@gmail.com>
2024-03-27 14:05:56 -0400

    update fig 4 results objects

--- doc/results-text-files.R
+++ doc/results-text-files.R
@@ -1,11 +1,10 @@
 library(tidyverse)
 ### Numbers for text
 ##load in text data
-load("results-text/Figure2_textdata.Rdata")
-load("results-text/Figure3_textdata.Rdata")
-load("results-text/Figure4_textdata.Rdata")
-allmort_awm <- read.csv("../data/processed_data/MortalityComposition.csv")
-load("results-text/Figure5_textdata.Rdata")
+load("doc/results-text/Figure2_textdata.Rdata")
+load("doc/results-text/Figure3_textdata.Rdata")
+load("doc/results-text/Figure4_textdata.Rdata")
+load("doc/results-text/Figure5_textdata.Rdata")
 
 ##Figure 2
 
@@ -127,85 +126,85 @@ awr_wholePlot_2023 <- fig3_textresults %>% filter(Census == "4" & Group == "3" &
 
 #Averages
 
-avg_fram <- allmort_awm %>% filter(sp == "fram") %>% pull(abgmort) %>% 
-  mean(na.rm = TRUE)
-avg_quve <- allmort_awm %>% filter(sp == "quve") %>% pull(abgmort) %>% 
-  mean(na.rm = TRUE)
-avg_quru <- allmort_awm %>% filter(sp == "quru") %>% pull(abgmort) %>%
-  mean(na.rm = TRUE)
-avg_qupr <- allmort_awm %>% filter(sp == "qupr") %>% pull(abgmort) %>% 
-  mean(na.rm = TRUE)
-avg_qual <- allmort_awm %>% filter(sp == "qual") %>% pull(abgmort) %>% 
-  mean(na.rm = TRUE)
-avg_litu <- allmort_awm %>% filter(sp == "litu") %>% pull(abgmort) %>% 
-  mean(na.rm = TRUE)
-avg_other <- allmort_awm %>%  mutate(plot_species = if_else(sp %in% top_sp$sp,sp, "Other")) %>% filter(plot_species == "Other") %>% pull(abgmort) %>% mean(na.rm = TRUE)
+avg_fram <- figure4_textresults %>% filter(plotspecies == "Fraxinus americana") %>% 
+  pull(mort_woody) %>% sum()/11
+avg_quve <- figure4_textresults %>% filter(plotspecies == "Quercus velutina") %>% 
+  pull(mort_woody) %>% sum()/11
+avg_quru <- figure4_textresults %>% filter(plotspecies == "Quercus rubra") %>% 
+  pull(mort_woody) %>% sum()/11
+avg_qupr <- figure4_textresults %>% filter(plotspecies == "Quercus prinus") %>% 
+  pull(mort_woody) %>% sum()/11
+avg_qual <- figure4_textresults %>% filter(plotspecies == "Quercus alba") %>% 
+  pull(mort_woody) %>% sum()/11
+avg_litu <- figure4_textresults %>% filter(plotspecies == "Liriodendron tulipifera") %>% 
+  pull(mort_woody) %>% sum()/11
+avg_other <- figure4_textresults %>% filter(plotspecies == "Other") %>% 
+  pull(mort_woody) %>% sum()/11
 
 
-#5 Year Averages for top three species -- Currently calculated with survey_years 2014-2023
+#Five Year Averages for top three species -- Calculated with survey_years 2014-2023
 
-avg_fram_first5 <- allmort_awm %>% filter(sp == "fram" & (survey_year == "2014" | survey_year == "2015" | survey_year == "2016" | survey_year == "2017" | survey_year == "2018")) %>% pull(abgmort) %>% mean(na.rm = TRUE)
+avg_fram_first5 <- figure4_textresults %>% filter(plotspecies == "Fraxinus americana" & (survey_year == "2014" | survey_year == "2015" | survey_year == "2016" | survey_year == "2017" | survey_year == "2018")) %>% pull(mort_woody) %>% mean(na.rm = TRUE)
 
-avg_fram_second5 <- allmort_awm %>% filter(sp == "fram" & (survey_year == "2019" | survey_year == "2020" | survey_year == "2021" | survey_year == "2022" | survey_year == "2023")) %>% pull(abgmort) %>% mean(na.rm = TRUE)
+avg_fram_second5 <- figure4_textresults %>% filter(plotspecies == "Fraxinus americana" & (survey_year == "2019" | survey_year == "2020" | survey_year == "2021" | survey_year == "2022" | survey_year == "2023")) %>% pull(mort_woody) %>% mean(na.rm = TRUE)
 
-avg_quve_five5 <- allmort_awm %>% filter(sp == "quve" & (survey_year == "2014" | survey_year == "2015" | survey_year == "2016" | survey_year == "2017" | survey_year == "2018")) %>% pull(abgmort) %>% mean(na.rm = TRUE)
+avg_quve_first5 <- figure4_textresults %>% filter(plotspecies == "Quercus velutina" & (survey_year == "2014" | survey_year == "2015" | survey_year == "2016" | survey_year == "2017" | survey_year == "2018")) %>% pull(mort_woody) %>% mean(na.rm = TRUE)
 
-avg_quve_second5 <- allmort_awm %>% filter(sp == "quve" & (survey_year == "2019" | survey_year == "2020" | survey_year == "2021" | survey_year == "2022" | survey_year == "2023")) %>% pull(abgmort) %>% mean(na.rm = TRUE)
+avg_quve_second5 <- figure4_textresults %>% filter(plotspecies == "Quercus velutina" & (survey_year == "2019" | survey_year == "2020" | survey_year == "2021" | survey_year == "2022" | survey_year == "2023")) %>% pull(mort_woody) %>% mean(na.rm = TRUE)
 
-avg_quru_first5 <-  allmort_awm %>% filter(sp == "quru" & (survey_year == "2014" | survey_year == "2015" | survey_year == "2016" | survey_year == "2017" | survey_year == "2018")) %>% pull(abgmort) %>% mean(na.rm = TRUE)
+avg_quru_first5 <- figure4_textresults %>% filter(plotspecies == "Quercus rubra" & (survey_year == "2014" | survey_year == "2015" | survey_year == "2016" | survey_year == "2017" | survey_year == "2018")) %>% pull(mort_woody) %>% mean(na.rm = TRUE)
 
-avg_quru_second5 <- allmort_awm %>% filter(sp == "quru" & (survey_year == "2019" | survey_year == "2020" | survey_year == "2021" | survey_year == "2022" | survey_year == "2023")) %>% pull(abgmort) %>% mean(na.rm = TRUE)
+avg_quru_second5 <- figure4_textresults %>% filter(plotspecies == "Quercus rubra" & (survey_year == "2019" | survey_year == "2020" | survey_year == "2021" | survey_year == "2022" | survey_year == "2023")) %>% pull(mort_woody) %>% mean(na.rm = TRUE)
 
 
 #Peak for each species
 
-fram_peak <-  allmort_awm %>% filter(sp == "fram") %>% pull(abgmort) %>% max(na.rm = TRUE)
+fram_peak <- figure4_textresults %>% filter(plotspecies == "Fraxinus americana") %>% pull(mort_woody) %>% max()
 
-quve_peak <- allmort_awm %>% filter(sp == "quve") %>% pull(abgmort) %>% max(na.rm = TRUE)
-  
-quru_peak <- allmort_awm %>% filter(sp == "quru") %>% pull(abgmort) %>% max(na.rm = TRUE)
+quve_peak <- figure4_textresults %>% filter(plotspecies == "Quercus velutina") %>% pull(mort_woody) %>% max()
 
-qupr_peak <- allmort_awm %>% filter(sp == "qupr") %>% pull(abgmort) %>% max(na.rm = TRUE)
+quru_peak <- figure4_textresults %>% filter(plotspecies == "Quercus rubra") %>% pull(mort_woody) %>% max()
 
-qual_peak <- allmort_awm %>% filter(sp == "quru") %>% pull(abgmort) %>% max(na.rm = TRUE)
+qupr_peak <- figure4_textresults %>% filter(plotspecies == "Quercus prinus") %>% pull(mort_woody) %>% max()
 
-litu_peak <- allmort_awm %>% filter(sp == "litu") %>% pull(abgmort) %>% max(na.rm = TRUE)
+qual_peak <- figure4_textresults %>% filter(plotspecies == "Quercus alba") %>% pull(mort_woody) %>% max()
 
-other_peak <- allmort_awm %>%  mutate(plot_species = if_else(sp %in% top_sp$sp,sp, "Other")) %>% filter(plot_species == "Other") %>% pull(abgmort) %>% max(na.rm = TRUE)
+litu_peak <- figure4_textresults %>% filter(plotspecies == "Liriodendron tulipifera") %>% pull(mort_woody) %>% max()
 
+other_peak <- figure4_textresults %>% filter(plotspecies == "Other") %>% pull(mort_woody) %>% max()
 
 ##Figure 5
 
 #Total number of recruits in canopy and understory for each region
 
 #Canopy
-group1_canopy <- fig5_textresults %>% filter(Group == "1" & canopy_position == "canopy")%>% pull(n_stems) %>% sum(na.rm = TRUE)
-group2_canopy <- fig5_textresults %>% filter(Group == "2" & canopy_position == "canopy")%>% pull(n_stems) %>% sum(na.rm = TRUE)
-group3_canopy <- fig5_textresults %>% filter(Group == "3" & canopy_position == "canopy")%>% pull(n_stems) %>% sum(na.rm = TRUE)
+group1_canopy <- figure5_textresults %>% filter(Group == "1" & canopy_position == "canopy")%>% pull(n_stems) %>% sum(na.rm = TRUE)
+group2_canopy <- figure5_textresults %>% filter(Group == "2" & canopy_position == "canopy")%>% pull(n_stems) %>% sum(na.rm = TRUE)
+group3_canopy <- figure5_textresults %>% filter(Group == "3" & canopy_position == "canopy")%>% pull(n_stems) %>% sum(na.rm = TRUE)
 
 #Understory
-group1_understory <- fig5_textresults %>% filter(Group == "1" & canopy_position == "understory")%>% pull(n_stems) %>% sum(na.rm = TRUE)
-group2_understory <- fig5_textresults %>% filter(Group == "2" & canopy_position == "understory")%>% pull(n_stems) %>% sum(na.rm = TRUE)
-group3_understory <- fig5_textresults %>% filter(Group == "3" & canopy_position == "understory")%>% pull(n_stems) %>% sum(na.rm = TRUE)
+group1_understory <- figure5_textresults %>% filter(Group == "1" & canopy_position == "understory")%>% pull(n_stems) %>% sum(na.rm = TRUE)
+group2_understory <- figure5_textresults %>% filter(Group == "2" & canopy_position == "understory")%>% pull(n_stems) %>% sum(na.rm = TRUE)
+group3_understory <- figure5_textresults %>% filter(Group == "3" & canopy_position == "understory")%>% pull(n_stems) %>% sum(na.rm = TRUE)
 
 #Number of recruits for oak spp, hickory spp, libe and astr in each of the groups
 
 #Oak recruits
-group1_oakRecruit <- fig5_textresults %>% filter(Group == "1" & plot_sp == "Quercus spp.")%>% pull(n_stems)
-group2_oakRecruit <- fig5_textresults %>% filter(Group == "2" & plot_sp == "Quercus spp.") %>% pull(n_stems)
-group3_oakRecruit <- fig5_textresults %>% filter(Group == "3" & plot_sp == "Quercus spp.")%>% pull(n_stems)
+group1_oakRecruit <- figure5_textresults %>% filter(Group == "1" & plot_sp == "Quercus spp.")%>% pull(n_stems)
+group2_oakRecruit <- figure5_textresults %>% filter(Group == "2" & plot_sp == "Quercus spp.") %>% pull(n_stems)
+group3_oakRecruit <- figure5_textresults %>% filter(Group == "3" & plot_sp == "Quercus spp.")%>% pull(n_stems)
 
 #Hickory Recruits
-group1_hickoryRecruit <- fig5_textresults %>% filter(Group == "1" & plot_sp == "Carya spp.")%>% pull(n_stems)
-group2_hickoryRecruit <- fig5_textresults %>% filter(Group == "2" & plot_sp == "Carya spp.")%>% pull(n_stems)
-group2_hickoryRecruit <- fig5_textresults %>% filter(Group == "3" & plot_sp == "Carya spp.")%>% pull(n_stems)
+group1_hickoryRecruit <- figure5_textresults %>% filter(Group == "1" & plot_sp == "Carya spp.")%>% pull(n_stems)
+group2_hickoryRecruit <- figure5_textresults %>% filter(Group == "2" & plot_sp == "Carya spp.")%>% pull(n_stems)
+group2_hickoryRecruit <- figure5_textresults %>% filter(Group == "3" & plot_sp == "Carya spp.")%>% pull(n_stems)
 
 #libe Recruits
-group1_libeRecruit <- fig5_textresults %>% filter(Group == "1" & plot_sp == "Lindera benzoin") %>% pull(n_stems)
-group2_libeRecruit <- fig5_textresults %>% filter(Group == "2" & plot_sp == "Lindera benzoin") %>% pull(n_stems)
-group3_libeRecruit <- fig5_textresults %>% filter(Group == "3" & plot_sp == "Lindera benzoin") %>% pull(n_stems)
+group1_libeRecruit <- figure5_textresults %>% filter(Group == "1" & plot_sp == "Lindera benzoin") %>% pull(n_stems)
+group2_libeRecruit <- figure5_textresults %>% filter(Group == "2" & plot_sp == "Lindera benzoin") %>% pull(n_stems)
+group3_libeRecruit <- figure5_textresults %>% filter(Group == "3" & plot_sp == "Lindera benzoin") %>% pull(n_stems)
 
 #astr Recruits
-group1_astrRecruit <- fig5_textresults %>% filter(Group == "1" & plot_sp == "Asimina triloba") %>% pull(n_stems)
-group2_astrRecruit <- fig5_textresults %>% filter(Group == "2" & plot_sp == "Asimina triloba") %>% pull(n_stems)
-group3_astrRecruit <- fig5_textresults %>% filter(Group == "3" & plot_sp == "Asimina triloba") %>% pull(n_stems)
+group1_astrRecruit <- figure5_textresults %>% filter(Group == "1" & plot_sp == "Asimina triloba") %>% pull(n_stems)
+group2_astrRecruit <- figure5_textresults %>% filter(Group == "2" & plot_sp == "Asimina triloba") %>% pull(n_stems)
+group3_astrRecruit <- figure5_textresults %>% filter(Group == "3" & plot_sp == "Asimina triloba") %>% pull(n_stems)
